19cad9db813a00d1d7e54f9bcda4fad1c57a31ae
LoeiFy <LoeiFy@gmail.com>
2018-11-03 20:39:38 +0800

    locale

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-const getLanguages = require('./lib/languages')
+const getLocale = require('./lib/locale')
 const parser = require('./lib/parser')
 
 module.exports = class {
-  constructor(directory, locale = 'default') {
-    this.directory = directory
-    this.languages = {}
-    this.language = locale
+  constructor(dir, locale) {
+    this.dir = dir
+    this.localeData = locale ? getLocale(dir, locale) : {}
+    this.currentLocale = locale
   }
 
-  set locale(language) {
-    this.languages = getLanguages(this.directory)
-    this.language = language
+  set locale(locale) {
+    this.localeData = getLocale(this.dir, locale)
+    this.currentLocale = locale
   }
 
   get locale() {
-    return this.language
+    return this.currentLocale
   }
 
-  getLocale() {
-    const locale = this.languages[this.language]
-    if (!locale) {
-      return {}
-    }
-    return locale
+  __(...args) {
+    return parser('_', this.localeData, ...args)
   }
 
-  __(vars, ...params) {
-    return parser('_', this.getLocale(), vars, ...params)
-  }
-
-  _n(vars, ...params) {
-    return parser('n', this.getLocale(), vars, ...params)
+  _n(...args) {
+    return parser('n', this.localeData, ...args)
   }
 }
--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
 const { join } = require('path')
 const I18n = require('../')
 
-const i18n = new I18n(join(__dirname, 'locales'))
+const i18n = new I18n(join(__dirname, 'locales'), 'en')
 
 i18n.locale = 'default'
 
+console.log(i18n.locale)
 console.log(i18n.__('three', 'aaaa'))
 console.log(i18n.__('a.b.c', 'aaaa', 'bbbb'))
 console.log(i18n._n('num', 0))
